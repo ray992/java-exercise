c3c36e97bb8ea12ac456a2e5cdd5c12c94ce5abf
叶强 <ye0917@firestorm-sea.com>
2020-10-03 21:54:07 +0700

    初始化pom.xml

---
+++ src/main/java/com/exercise/io/FileDemo.java
@@ -0,0 +1,21 @@
+package com.exercise.io;
+
+import java.io.File;
+
+/**
+ * @ClassName FileDemo
+ * @Description File 方法
+ * @Author yeqiang
+ * @Date 2020/10/3 8:28 下午
+ * @Version 1.0
+ **/
+public class FileDemo {
+
+    public static void createFile(String fileName){
+        File file1 = new File(fileName);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

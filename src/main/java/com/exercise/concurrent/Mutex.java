60faad15f66d07d1700d15258659187f8e461d0e
叶强 <ye0917@firestorm-sea.com>
2020-10-08 17:26:44 +0700

    synchronized关键字的demo测试

--- src/main/java/com/exercise/concurrent/Mutex.java
+++ src/main/java/com/exercise/concurrent/Mutex.java
@@ -1,5 +1,7 @@
 package com.exercise.concurrent;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @ClassName Mutex
  * @Description TODO
@@ -8,4 +10,23 @@ package com.exercise.concurrent;
  * @Version 1.0
  **/
 public class Mutex {
+
+    private static final Object MUTEX = new Object();
+
+    public void access(){
+        synchronized (MUTEX){
+            try {
+                TimeUnit.MINUTES.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        final Mutex mutex = new Mutex();
+        for (int i = 0; i < 5; i++){
+            new Thread(mutex::access).start();
+        }
+    }
 }
--- src/main/java/com/exercise/concurrent/SynchronizedDemo.java
+++ src/main/java/com/exercise/concurrent/SynchronizedDemo.java
@@ -1,11 +1,48 @@
 package com.exercise.concurrent;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @ClassName SynchronizedDemo
- * @Description TODO
+ * @Description Synchronized关键字的使用
  * @Author yeqiang
  * @Date 2020/10/7 10:10 下午
  * @Version 1.0
  **/
 public class SynchronizedDemo {
+
+    public static void main(String[] args) {
+        final TicketWindowRunnable ticketWindowRunnable = new TicketWindowRunnable();
+        Thread t1 = new Thread(ticketWindowRunnable, "n1");
+        Thread t2 = new Thread(ticketWindowRunnable, "n2");
+        Thread t3 = new Thread(ticketWindowRunnable, "n3");
+        Thread t4 = new Thread(ticketWindowRunnable, "n4");
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
+
+}
+class TicketWindowRunnable implements Runnable{
+
+    private int index = 1;
+
+    private final static int MAX = 500;
+
+    private final static Object MUTEX = new Object();
+
+    @Override
+    public void run() {
+        synchronized (MUTEX){
+            while (index <= MAX){
+                System.out.println(Thread.currentThread().getName() + " 号码: "+ index++);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }

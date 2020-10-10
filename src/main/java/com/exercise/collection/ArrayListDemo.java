c180ca6e0cde5d0f2794fc5959cf0d0e6fb58e61
叶强 <ye0917@firestorm-sea.com>
2020-10-10 13:16:54 +0700

    增加ArrayList 遍历测试

--- src/main/java/com/exercise/collection/ArrayListDemo.java
+++ src/main/java/com/exercise/collection/ArrayListDemo.java
@@ -1,5 +1,9 @@
 package com.exercise.collection;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  * @ClassName ArrayListDemo
  * @Description TODO
@@ -8,4 +12,74 @@ package com.exercise.collection;
  * @Version 1.0
  **/
 public class ArrayListDemo {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 100000; i ++){
+            list.add(i);
+        }
+        randomAccess(list);
+        iteratorAccess(list);
+        forAccess(list);
+        //Java不支持向下转型
+        List<String> stringList = new ArrayList<>();
+        stringList.add("a");
+        stringList.add("b");
+        stringList.add("c");
+        stringList.add("d");
+        String[] array = stringList.toArray(new String[0]);
+        String[] array2= new String[stringList.size()];
+        stringList.toArray(array2);
+        System.out.println(array.length);
+        System.out.println(array2.length);
+    }
+
+    /**
+    * @Name randomAccess
+    * @Description 随机访问
+    * @Date 2020/10/9 8:57 上午
+    * @params [list]
+    * @return void
+    **/
+    public static void randomAccess(List list){
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < list.size(); i++){
+            list.get(i);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("randomAccessTime: "+(endTime-startTime));
+    }
+
+    /**
+    * @Name iteratorAccess
+    * @Description 迭代器访问
+    * @Date 2020/10/9 8:58 上午
+    * @params [list]
+    * @return void
+    **/
+    public static void iteratorAccess(List list){
+        long startTime = System.currentTimeMillis();
+        ListIterator listIterator = list.listIterator();
+        while (listIterator.hasNext()){
+            listIterator.next();
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("iteratorAccess: "+(endTime-startTime));
+    }
+
+    /**
+    * @Name forAccess
+    * @Description for循环访问
+    * @Date 2020/10/9 8:59 上午
+    * @params [list]
+    * @return void
+    **/
+    public static void forAccess(List list){
+        long startTime = System.currentTimeMillis();
+        for (Object o:list){
+
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("forAccess: "+(endTime-startTime));
+    }
 }

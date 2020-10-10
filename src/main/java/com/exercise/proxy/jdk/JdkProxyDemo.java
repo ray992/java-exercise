22a5b55d246d05d42250081c84f34a946935dba5
叶强 <ye0917@firestorm-sea.com>
2020-10-10 10:58:20 +0700

    jdk动态代理

--- src/main/java/com/exercise/proxy/jdk/JdkProxyDemo.java
+++ src/main/java/com/exercise/proxy/jdk/JdkProxyDemo.java
@@ -1,11 +1,24 @@
 package com.exercise.proxy.jdk;
 
+import com.exercise.proxy.Star;
+import com.exercise.proxy.Ll;
+
 /**
  * @ClassName JdkProxyDemo
- * @Description TODO
+ * @Description jdk代理测试
  * @Author yeqiang
  * @Date 2020/10/10 8:50 上午
  * @Version 1.0
  **/
 public class JdkProxyDemo {
+
+    public static void main(String[] args) {
+        Star star = new Ll();
+        StarProxy starProxy = new StarProxy();
+        starProxy.setObject(star);
+        Object proxyObject = starProxy.createProxyObject();
+        Star proxyStar = (Star)proxyObject;
+        proxyStar.dance("aa");
+    }
+    //代理类不能直接包含被代理的对象， 而是一个InvocationHandler, InvocationHandler包含被代理的对象， 并负责分发请求给代理对象， 分发前后都可以做增强
 }
--- src/main/java/com/exercise/proxy/jdk/StarProxy.java
+++ src/main/java/com/exercise/proxy/jdk/StarProxy.java
@@ -1,11 +1,34 @@
 package com.exercise.proxy.jdk;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 /**
  * @ClassName StarProxy
- * @Description TODO
+ * @Description jdk代理类定义
  * @Author yeqiang
  * @Date 2020/10/10 8:46 上午
  * @Version 1.0
  **/
-public class StarProxy {
+public class StarProxy implements InvocationHandler {
+
+    //目标类
+    private Object object;
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("earn money"); //增强
+        method.invoke(object, args);
+        return null;
+    }
+
+    //生成代理类
+    public Object createProxyObject(){
+        return Proxy.newProxyInstance(object.getClass().getClassLoader(), object.getClass().getInterfaces(), this);
+    }
 }
